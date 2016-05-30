9899e8920cf8ebda7d71084abfcbae9a4394af78
Wei Liang <weiliang.c3@gmail.com>
2016-05-30 12:40:11 +0100

    done some work?

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ var bcrypt   = require('bcrypt-nodejs');
 var userSchema = mongoose.Schema({
   local: {
     username: { type: String },
+    fullname: { type: String },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true }
   },
